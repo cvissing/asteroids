cb3ab95e6d555c7373e2864e830cd2c9dd177903
cvissing <cvissing@mac.com>
2024-10-18 23:37:20 -0400

    Updated main.py

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
@@ -18,6 +24,12 @@ def main():
         pygame.Surface.fill(screen, "black")
         player.draw(screen)
         player.update(dt)
+
+        for update in updateable:
+            update.update(dt)
+
+        for draw in drawable:
+            draw.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000

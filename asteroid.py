02e370ed58bc08245142105420c8b15f9603d2e7
cvissing <cvissing@mac.com>
2024-10-20 18:53:01 -0400

    Updated asteroid.py

--- asteroid.py
+++ asteroid.py
@@ -4,9 +4,7 @@ from circleshape import CircleShape
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
-        self.x = x
-        self.y = y
-        self.radius = radius
+        super().__init__(x, y, radius)
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)

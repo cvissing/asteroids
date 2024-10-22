4cce22bbd3acdbb0b67ffd18a2ec130d27241c3a
cvissing <cvissing@mac.com>
2024-10-22 11:04:23 -0400

    Updated main.py

--- main.py
+++ main.py
@@ -41,6 +41,11 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    shot.kill()
+                    asteroid.kill()
+
         pygame.Surface.fill(screen, "black")
 
         for obj in drawable:
